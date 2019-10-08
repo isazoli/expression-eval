c315aabf3086550e9627c2c8c998ed6419c36490
isaszegi1zo628 <isaszegi.zoltan@telekom.hu>
2019-10-08 14:59:45 +0200

    Add property-based tests

--- src/test/scala/ArithParserTest.scala
+++ src/test/scala/ArithParserTest.scala
@@ -1,29 +1,53 @@
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import org.scalatest.{Matchers, PropSpec}
 
-class ParserCombinatorTest extends FunSpec with Matchers {
+import scala.math.{cos, sin}
 
-  describe("parser combinator should parse") {
+class ArithParserTest extends PropSpec with Matchers {
+
+  property("parsing and evaluating should work for valid cases") {
     val combinator = new ArithParser with SmartAST {}
-//    it("+2") {
-//      combinator.parseAll(
-//        combinator.expr, "+2"
-//      ).get shouldBe 4D
-//    }
-//    it("-2") {
-//      combinator.parseAll(
-//        combinator.expr, "-2"
-//      ).get.eval shouldBe -2D
-//    }
-    it("3 + 2") {
-      combinator.parseAll(
-        combinator.expr, "3 + 2"
-      ).get.eval shouldBe 5D
-    }
-    it("3 + 2 + 4") {
-      combinator.parseAll(
-        combinator.expr, "3 + 2 + 4"
-      ).get.eval shouldBe 9D
+    forEvery(ArithParserTest.validCasesWithExpectedResult) {
+      (expression: String, expectedValue: Double) =>
+        val value = combinator.parseAll(combinator.expr, expression)
+        value.successful shouldBe true
+        value.get.eval shouldBe expectedValue
     }
   }
+
+  property("parsing should fail for invalid cases") {
+    val combinator = new ArithParser with SmartAST {}
+    forEvery(ArithParserTest.inValidCases) {
+      expression: String =>
+        val value = combinator.parseAll(combinator.expr, expression)
+        value.successful shouldBe false
+    }
+  }
+
+}
+
+object ArithParserTest {
+
+  private val validCasesWithExpectedResult = Table(
+    "expression" -> "expectedValue",
+    List(
+      "3 + 2 + 4" -> (3 + 2 + 4).toDouble,
+      // TODO: add sing operations
+      //"+2" -> 2D,
+      //"-2" -> -2D,
+      "sin(30) + cos(20)" -> (sin(30) + cos(20)),
+      "sin(sin(30) + cos(20))" -> sin(sin(30) + cos(20)),
+      "3 + sin(sin(30) + cos(20))" -> (3 + sin(sin(30) + cos(20)))
+    ): _*
+  )
+
+  private val inValidCases = Table(
+    "expression",
+    List(
+      "sin(30",
+      "3 + +",
+      "3+"
+    ): _*
+  )
 
 }
